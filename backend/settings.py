e1d7cac714746b70652ace93c9efcd3004386e74
Mounsef Litniti <93012372+mounseflit@users.noreply.github.com>
2023-05-19 19:28:41 +0100

    Add files via upload

--- backend/settings.py
+++ backend/settings.py
@@ -122,9 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/STATIC/'
+STATIC_URL = 'STATIC/'
 STATICFILES_DIR=[os.path.join(BASE_DIR,"STATIC")]
-STATICFILES_DIR=[os.path.join("","STATIC")]
 MEDIA_URL= '/images/'
 
 
