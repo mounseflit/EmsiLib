8703c26a6ab164126cbea3e55e401817bff20871
Mounsef Litniti <93012372+mounseflit@users.noreply.github.com>
2023-07-14 02:19:16 +0100

    Delete books directory

--- books/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Livre, Reservation, Emprunt, Commentaire
-
-# Register your models here.
-admin.site.register(Livre)
-admin.site.register(Reservation)
-admin.site.register(Emprunt)
-admin.site.register(Commentaire)
